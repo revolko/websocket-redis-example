3290b779376dcbb3b90c019e5530d9c9d44041f6
Juraj Paluba <juraj.palubajp@gmail.com>
2024-11-25 10:33:18 +0100

    feat: initialize redis server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use redis;
+use redis::Commands;
+
 fn main() {
-    println!("Hello, world!");
+    let client = redis::Client::open("redis://127.0.0.1").unwrap();
+    let mut con = client.get_connection().unwrap();
+
+    let _: () = con.set("key", 42).unwrap();
+    let key: i32 = con.get("key").unwrap();
+    println!("{}", key);
 }
